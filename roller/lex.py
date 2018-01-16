d42c764437b8b0065c833a5f23a4f7aea78eee35
Hayden Muhl <haydenmuhl@gmail.com>
2018-01-15 22:07:06 -0800

    Lexer done

--- roller/lex.py
+++ roller/lex.py
@@ -10,12 +10,21 @@ class Lexer:
 
   def __init__(self, input):
     self.input = input
+    self.current_token = None
 
-  def next_token(self):
-    return ''
+  def token(self):
+    return self.current_token
 
   def advance(self):
     for token_type in Lexer.regexes:
       result = token_type[1].match(self.input)
       if result != None:
-        return token_type[0], result.group(0)
+        token = result.group(0)
+        self.input = self.input[len(token):]
+        self.current_token = (token_type[0], token)
+        return self.current_token
+    if len(self.input) == 0:
+      self.current_token = ('EOI', None)
+      return self.current_token
+    else:
+      raise Exception
--- tests/test_lex.py
+++ tests/test_lex.py
@@ -21,3 +21,40 @@ class TestLex(unittest.TestCase):
   def test_read_start(self):
     lexer = roller.lex.Lexer('-123')
     self.assertEqual(lexer.advance(), ('MINUS', '-'))
+
+  def test_multiple_tokens(self):
+    lexer = roller.lex.Lexer('123abc')
+    self.assertEqual(lexer.advance(), ('NUM', '123'))
+    self.assertEqual(lexer.advance(), ('STR', 'abc'))
+
+  def test_end_of_input(self):
+    lexer = roller.lex.Lexer('5+6')
+    self.assertEqual(lexer.advance(), ('NUM', '5'))
+    self.assertEqual(lexer.advance(), ('PLUS', '+'))
+    self.assertEqual(lexer.advance(), ('NUM', '6'))
+    self.assertEqual(lexer.advance(), ('EOI', None))
+
+  def test_illegal_syntax(self):
+    lexer = roller.lex.Lexer('foo(123)')
+    self.assertEqual(lexer.advance(), ('STR', 'foo'))
+    with self.assertRaises(Exception):
+      lexer.advance()
+
+  def test_token_uninitialized(self):
+    lexer = roller.lex.Lexer('asdf')
+    self.assertEqual(lexer.token(), None)
+
+  def test_initialized(self):
+    lexer = roller.lex.Lexer('a+b')
+    self.assertEqual(lexer.advance(), ('STR', 'a'))
+    self.assertEqual(lexer.token(), ('STR', 'a'))
+    self.assertEqual(lexer.advance(), ('PLUS', '+'))
+    self.assertEqual(lexer.token(), ('PLUS', '+'))
+
+  def test_token_end_of_input(self):
+    lexer = roller.lex.Lexer('a1')
+    self.assertEqual(lexer.advance(), ('STR', 'a'))
+    self.assertEqual(lexer.advance(), ('NUM', '1'))
+    self.assertEqual(lexer.advance(), ('EOI', None))
+    self.assertEqual(lexer.token(), ('EOI', None))
+
